7a4fcb382f760f3a9d7e94fa9c02c78cd66aef2a
Daniel Fang <danielfang@hotmail.com>
2023-07-15 22:56:32 -0700

    Updates

--- src/components/AboutModal/aboutContent.jsx
+++
@@ -1,6 +0,0 @@
-import { Modal } from "antd";
-import React from "react";
-
-export const AristaContent = () => {
-  return <div className='modal-content'>Arista stuff here</div>;
-};
--- src/components/Navbar/AnimatedIcons.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const animatedIcon = (icon) => {
-  const [hovered, setHovered] = <div>{icon}</div>;
-};
